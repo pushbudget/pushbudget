2248a5e4aa62c65db9bd34c5c2368ecc26d62554
Eric Lewin <Eric Lewin>
2016-01-10 14:54:18 -0600

    added ionic files

---
+++ www/js/app.js
@@ -0,0 +1,86 @@
+// Push Budget
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('main', {
+    url: '/main',
+    abstract: true,
+    templateUrl: 'templates/main.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('main.home', {
+    url: '/home',
+    views: {
+      'home': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('main.trans', {
+      url: '/trans',
+      views: {
+        'transactions': {
+          templateUrl: 'templates/transactions.html',
+          controller: 'transCtrl'
+        }
+      }
+    })
+
+    .state('main.budgets', {
+      url: '/budgets',
+      views: {
+        'budgets': {
+          templateUrl: 'templates/budgets.html',
+          controller: 'budgetCtrl'
+        }
+      }
+    })
+
+  .state('main.account', {
+    url: '/account',
+    views: {
+      'profile': {
+        templateUrl: 'templates/account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/main/home');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,25 @@
+angular.module('starter.controllers', [])
+
+.controller('homeCtrl', function($scope) {})
+
+.controller('transCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+
+
+})
+
+.controller('budgetCtrl', function($scope) {})
+
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});

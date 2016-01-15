1147b1439a6cb1dd5002d9eb62c1ec2ffdbd773d
Jared Hensley <jahensle85@me.com>
2016-01-15 11:35:47 -0600

    transaction service working with dummy controller and a test view that could be useful or removed

--- www/js/services/transaction-client-service.js
+++ www/js/services/transaction-client-service.js
@@ -1,71 +1,81 @@
 angular.module('pushbudget').service('transactionService', function ($http, $q) {
 
+  console.log('hey im in here');
 
   // transaction endpoints
 
   // get all transactions for particular user
-
-
-  return {
-
-    this.getAllUserTransactions: function (userId) {
-      var dfd = $q.defer();
-      $http({
-          method: 'POST',
-          url: '/api/transactions/user/:userId'
-        }).then(function (res) {
-          dfd.resolve(res);
-        })
-        .catch(function (err) {
-          dfd.resolve(err);
-        });
-      return dfd.promise;
-    }
-
-
-
-    editGroup: function (id, admin, group) {
-        group.admin = admin;
-        var dfd = $q.defer();
-        $http({
-          method: 'PUT',
-          url: '/api/groups/' + id,
-          data: group
-        }).then(function (group) {
-          getGroup(id).then(function (response) {
-            dfd.resolve(response.data);
-          });
-        });
-        return dfd.promise;
-      },
-
-      this.getAllUserTransactions = function (userId) {
-        var dfd = $q.defer();
-        $http({
-
-          }).then(function (res) {
-            dfd.resolve(res);
-          })
-          .catch(function (err) {
-            dfd.resolve(err);
-          });
-      }
+  this.getAllUserTransactions = function (userId) {
+    var dfd = $q.defer();
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3001/api/transactions/user/' + userId
+      }).then(function (res) {
+        dfd.resolve(res);
+      })
+      .catch(function (err) {
+        dfd.reject(err);
+      });
     return dfd.promise;
-  },
+  }
 
-  this.editSpecificUserTransactions: function (userId) {
-      var dfd = $q.defer();
-      $http.patch()
-    },
+  // get specific transaction for user
+  this.getSpecificUserTransaction = function (transId) {
+    var dfd = $q.defer();
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3001/api/transactions/' + transId
+      }).then(function (res) {
+        dfd.resolve(res);
+      })
+      .catch(function (err) {
+        dfd.reject(err);
+      });
+    return dfd.promise;
+  }
 
-    this.deleteSpecificUserTransactions: function (userId) {
-      var dfd = $q.defer();
-      $http.delete()
-    },
+  // edit specific transaction
+  this.editSpecificUserTransaction = function (transId, editedTransaction) {
+    var dfd = $q.defer();
+    $http({
+        method: 'PATCH',
+        url: 'http://localhost:3001/api/transactions/' + transId,
+        data: editedTransaction
+      }).then(function (res) {
+        dfd.resolve(res);
+      })
+      .catch(function (err) {
+        dfd.reject(err);
+      });
+    return dfd.promise;
+  }
 
-    this.getAllUserUntagged: function (userId) {
-      var dfd = $q.defer();
-      $http.get()
-    }
-}
+  this.deleteSpecificUserTransaction = function (transId) {
+    var dfd = $q.defer();
+    $http({
+        method: 'DELETE',
+        url: 'http://localhost:3001/api/transactions/' + transId
+      }).then(function (res) {
+        dfd.resolve(res);
+      })
+      .catch(function (err) {
+        dfd.reject(err);
+      });
+    return dfd.promise;
+  }
+
+  this.getAllUserUntagged = function (userId) {
+    var dfd = $q.defer();
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3001/api/transactions/untagged/' + userId
+      }).then(function (res) {
+        dfd.resolve(res);
+      })
+      .catch(function (err) {
+        dfd.reject(err);
+      });
+    return dfd.promise;
+  }
+
 });

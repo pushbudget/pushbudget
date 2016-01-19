d6efa1758cddf173f1738cbbefb9137192cf7b3f
Brendan Doyle <bdoyle522@gmail.com>
2016-01-19 15:19:45 -0600

    list will sort based on dropdown menu and mock data we used

--- www/js/controllers/transactionsCtrl.js
+++ www/js/controllers/transactionsCtrl.js
@@ -1,11 +1,21 @@
-angular.module('pushbudget').controller('transCtrl', function ($scope, transactionService) {
+angular.module('pushbudget').controller('transCtrl', function ($scope, transactionService, userRef) {
 
     var testUserId = "5696bd87e4b07f04a7491c6b";
 
-    console.log($scope.currentUser);
-    console.log($scope.transactions);
+    console.log(userRef);
 
-    // Dropdown menu ng-options and default option
-    $scope.transactionsOptions = ['Untagged Transactions','Plaid Category', 'All Transactions', 'Food', 'Gas', 'Deposit'];
-    $scope.filterOption = $scope.transactionsOptions[1];
+    $scope.setOptions = function(){
+      $scope.transactionsOptions = [''];
+      console.log(userRef.budget.subbudgets);
+      for(var e in userRef.budget.subbudgets){
+        console.log(userRef.budget.subbudgets[e].category);
+        $scope.transactionsOptions.push(userRef.budget.subbudgets[e].category);
+      }
+      //$scope.filterOption = $scope.transactionsOptions[0];
+      // $scope.transactionOptions
+    }
+
+    $scope.setOptions();
+    // $scope.transactionsOptions = ['Untagged Transactions','Plaid Category', 'All Transactions', 'Food', 'Gas', 'Deposit'];
+
 });

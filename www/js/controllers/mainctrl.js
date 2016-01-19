1e851c98ebc2c25182e730edd8df23c4293dffbc
Jared Hensley <jahensle85@me.com>
2016-01-18 22:30:20 -0600

    current service urls updated

--- www/js/controllers/mainctrl.js
+++ www/js/controllers/mainctrl.js
@@ -1,12 +1,15 @@
-angular.module('pushbudget').controller('mainCtrl', function($scope, userService) {
+angular.module('pushbudget').controller('mainCtrl', function ($scope, userService) {
 
   //replace this later with a ref to the user which will be aquired during initial splash screen load
-  userService.getUserFromDb('5696bd87e4b07f04a7491c6b').then(function(res){
+  userService.getUserFromDb('5696bd87e4b07f04a7491c6b').then(function (res) {
     $scope.currentUser = res.data;
   });
 
-  $scope.updateUser = function(args){
+  $scope.updateUser = function (args) {
     //stuff
+    userService.getUserFromDb().then(function (res) {
+      $scope.currentUser = res.data;
+    })
   };
 
 

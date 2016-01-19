642fd7ef8df0a6de97e9a07cc084b90ceabc7f34
Brendan Doyle <bdoyle522@gmail.com>
2016-01-19 13:21:36 -0600

    userRef added, once we have structure of untagged transactions settled, will add filter

--- www/js/controllers/transactionsCtrl.js
+++ www/js/controllers/transactionsCtrl.js
@@ -1,11 +1,11 @@
 angular.module('pushbudget').controller('transCtrl', function ($scope, transactionService) {
 
     var testUserId = "5696bd87e4b07f04a7491c6b";
-    transactionService.getAllUserTransactions(testUserId).then(function(res){
-      $scope.transactions = res;
-    });
+
+    console.log($scope.currentUser);
+    console.log($scope.transactions);
 
     // Dropdown menu ng-options and default option
-    $scope.transactionsOptions = ['Untagged Transactions', 'All Transactions', 'Food', 'Gas', 'Entertainment'];
-    $scope.filterOption = $scope.transactionsOptions[0];
+    $scope.transactionsOptions = ['Untagged Transactions','Plaid Category', 'All Transactions', 'Food', 'Gas', 'Deposit'];
+    $scope.filterOption = $scope.transactionsOptions[1];
 });

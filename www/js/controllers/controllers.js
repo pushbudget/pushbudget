3deeb52c990870e3e3002ec9f71d978350fc5262
Brendan Doyle <bdoyle522@gmail.com>
2016-01-20 14:50:26 -0600

    no errors

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -3,6 +3,10 @@ angular.module('pushbudget.controllers', [])
 .controller('homeCtrl', function($scope) {})
 
 
+.controller('loginCtrl', function($scope) {})
+
+
+
 .controller('createCtrl', function($scope) {
 
 })
--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -1,7 +1,7 @@
-angular.module('pushbudget.controllers', []).controller('loginCtrl', function($scope, loginService) {
+angular.module('pushbudget').controller('loginCtrl', function($scope, loginService) {
       $scope.login = function(user){
         console.log(user);
-        userService.loginService(user).then(function(res){
+        loginService(user).then(function(res){
             console.log("loginctrl:", res);
         });
       };

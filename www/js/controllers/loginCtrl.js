d604258dd5f273bfe6d74bf6639ec841837910bf
Brendan Doyle <bdoyle522@gmail.com>
2016-01-20 14:40:22 -0600

    missed a character on index to load file

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -1,8 +1,8 @@
-angular.module('pushbudget.controllers', []).controller('loginCtrl', function($scope) {
+angular.module('pushbudget.controllers', []).controller('loginCtrl', function($scope, loginService) {
       $scope.login = function(user){
         console.log(user);
-        userService.loginUser(user).then(function(res){
+        userService.loginService(user).then(function(res){
             console.log("loginctrl:", res);
-        })
-      }
+        });
+      };
 })

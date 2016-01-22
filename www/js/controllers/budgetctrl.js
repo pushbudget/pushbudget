ee0ab7eef7bd0aed810faeb5dcdf15840d4c9915
Brendan Doyle <bdoyle522@gmail.com>
2016-01-22 15:26:50 -0600

    budget bars working with current data structure

--- www/js/controllers/budgetctrl.js
+++ www/js/controllers/budgetctrl.js
@@ -9,4 +9,19 @@ angular.module('pushbudget').controller('budgetCtrl', function($scope) {
     console.log("Label: ", points[0].label, " Value: ", points[0].value);
     //var activePoints = myRadar.getElementsAtEvent(evt);
   };
+
+  $scope.setWidth = function(subbudget){
+    // console.log(subbudget);
+    var totalSpent = 0;
+    for(var i = 0; i < subbudget.transactions.length; i++){
+      totalSpent += subbudget.transactions[i].amount;
+    }
+    if(totalSpent>subbudget.allocated){
+      return {width: '0%'};
+    }
+    var pct = ((1 - totalSpent/subbudget.allocated)*100).toString() + '%';
+    console.log(pct);
+    return {width: pct};
+  }
+
 });

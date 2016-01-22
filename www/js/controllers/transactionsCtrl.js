696ee1e131ecf61d2a2089782ecc3ab356c0047d
elewin <elewin@gmail.com>
2016-01-22 17:32:51 -0600

    refined array

--- www/js/controllers/transactionsCtrl.js
+++ www/js/controllers/transactionsCtrl.js
@@ -1,10 +1,14 @@
 angular.module('pushbudget').controller('transCtrl', function ($scope) {
 
-  $scope.allCategories = $scope.userSubBudgets.slice();
-  $scope.allCategories.unshift({
+  var subBudgets = $scope.userSubBudgets;
+  var allCategories = [{
     transactions: $scope.userUntagged,
     category: 'Untagged'
-  });
+  }];
+  for (var i = 0; i < subBudgets.length; i++){
+    allCategories.push(subBudgets[i]);
+  }
+  $scope.allCategories = allCategories;
   $scope.selectedSub = $scope.allCategories[0];
 
 

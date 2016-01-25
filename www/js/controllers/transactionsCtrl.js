77dce426aa5590598be369b9ab3ef0917aa4bf0a
Brendan Doyle <bdoyle522@gmail.com>
2016-01-25 12:26:12 -0600

    transactions hooked up to view

--- www/js/controllers/transactionsCtrl.js
+++ www/js/controllers/transactionsCtrl.js
@@ -1,15 +1,14 @@
 angular.module('pushbudget').controller('transCtrl', function ($scope) {
 
-  var subBudgets = $scope.userSubBudgets;
+  $scope.subBudgets = $scope.user.subbudgetArr;
   var allCategories = [{
-    transactions: $scope.userUntagged,
+    transactions: $scope.user.untaggedArr,
     category: 'Untagged'
   }];
-  for (var i = 0; i < subBudgets.length; i++){
-    allCategories.push(subBudgets[i]);
+  for (var i = 0; i < $scope.subBudgets.length; i++){
+    allCategories.push($scope.subBudgets[i]);
   }
   $scope.allCategories = allCategories;
   $scope.selectedSub = $scope.allCategories[0];
-
 
 });

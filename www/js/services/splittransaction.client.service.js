5f48b34052d394b7f825d444048cd94719443389
Jared Hensley <jahensle85@me.com>
2016-01-26 17:00:34 -0600

    here

--- www/js/services/splittransaction.client.service.js
+++ www/js/services/splittransaction.client.service.js
@@ -5,20 +5,17 @@ angular.module('pushbudget').service('splitTransaction', function ($http, $q, ab
   // only the child transctions, aka shadow or split, are added
   // to the subbudget splits array
 
-<<<<<<< HEAD
+
   this.addTransactionToBucket = function (transaction, subbudgetId) {
-=======
+
+  }
   this.addSplitTransaction = function (transaction, splits) {
->>>>>>> 813cf2e1b7a23e960cefb770c1b7f31670975c7c
+
     console.log(555555, transaction);
     var dfd = $q.defer();
     $http({
       method: 'POST',
-<<<<<<< HEAD
       url: absoluteUrl + 'api/split/' + subbudgetId,
-=======
-      url: absoluteUrl + 'api/split/',
->>>>>>> 813cf2e1b7a23e960cefb770c1b7f31670975c7c
       data: transaction
     }).then(function (bucket) {
       dfd.resolve(bucket)
